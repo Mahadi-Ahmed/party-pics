fa6cce2bbdbba6bd843c79cd0bfbc2c896c9333c
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-07-27 23:39:59 +0200

    add script to create qr code

---
+++ qrCode/qrCode.js
@@ -0,0 +1,20 @@
+const qrcode = require('qrcode');
+
+const url = 'https://dipti.mahadia.dev';
+const outputFile = 'qr_code.png';
+
+const options = {
+  errorCorrectionLevel: 'H',
+  type: 'png',
+  quality: 1,
+  margin: 2,
+  color: {
+    dark: '#000000',
+    light: '#FFFFFF'
+  }
+};
+
+qrcode.toFile(outputFile, url, options, (err) => {
+  if (err) throw err;
+  console.log(`QR code saved as ${outputFile}`);
+});

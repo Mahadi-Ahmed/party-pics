1df75dbf7eb9739d3f32b9315216e64068ee27af
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-07-31 19:49:09 +0200

    add some temporary basic style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,24 +9,20 @@
 	};
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div>
-	<form enctype="multipart/form-data">
-		<input
-			type="file"
-			name="fileToUpload"
-			multiple
-			accept=".jpg, .jpeg, .png, .heif, .heic, .dng, .tiff"
-			class="fileInput"
-			bind:this={fileInput}
-		/>
-		<Button on:click={handleClick}>Click me</Button>
-	</form>
+<div class="flex flex-col items-center gap-2 m-2">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<div>
+		<form enctype="multipart/form-data">
+			<input
+				type="file"
+				name="fileToUpload"
+				multiple
+				accept=".jpg, .jpeg, .png, .heif, .heic, .dng, .tiff"
+				bind:this={fileInput}
+				class="hidden"
+			/>
+			<Button on:click={handleClick}>Click me</Button>
+		</form>
+	</div>
 </div>
-
-<style>
-	.fileInput {
-		display: none;
-	}
-</style>

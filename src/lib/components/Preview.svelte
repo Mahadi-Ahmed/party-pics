ac978619987edc78de9473bc9b8aa69a72a72258
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-08-02 00:09:17 +0200

    wip preview component

--- src/lib/components/Preview.svelte
+++ src/lib/components/Preview.svelte
@@ -3,14 +3,24 @@
 	export let previewFiles: string[] = [];
 </script>
 
-<Carousel.Root class="" orientation="vertical">
-	<Carousel.Content class="">
-		{#each previewFiles as preview}
-			<Carousel.Item>
-				<img src={preview} alt="preview" />
-			</Carousel.Item>
-		{/each}
-	</Carousel.Content>
-	<Carousel.Previous />
-	<Carousel.Next />
-</Carousel.Root>
+<div class="relative w-full pb-12 pt-12">
+	<Carousel.Root 
+    opts={{
+      align: "center"
+    }}
+    orientation="vertical"
+    class="w-full max-w-xs mx-auto" 
+  >
+		<Carousel.Content>
+			{#each previewFiles as preview}
+				<Carousel.Item class="basis-full">
+          <div class="">
+					<img src={preview} alt="preview" class="w-full h-full object-cover rounded-md" />
+          </div>
+				</Carousel.Item>
+			{/each}
+		</Carousel.Content>
+		<Carousel.Previous />
+		<Carousel.Next />
+	</Carousel.Root>
+</div>

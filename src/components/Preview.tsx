2b2a3540c8bb016df30a793c01a86cf424562e9b
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-08-03 01:08:37 +0200

    use embla carousel for preview

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,21 +1,26 @@
+import useEmblaCarousel from 'embla-carousel-react'
 import type { PreviewFile } from '@/App'
 
 interface Props {
   previewFiles: PreviewFile[]
 }
 export const Preview = ({ previewFiles }: Props) => {
+  const [emblaRef] = useEmblaCarousel()
+
   return (
-    <>
-      <div className='grid grid-cols-3 gap-4'>
+    <div className='overflow-hidden' ref={emblaRef}>
+      <div className='flex'>
         {previewFiles.map((file, index) => (
-          <img
-            key={index}
-            src={file.url}
-            alt={`preview ${index}`}
-            className='aspect-square h-full w-full object-cover' />
+          <div className='flex-none w-full min-w-0'>
+            <img
+              key={index}
+              src={file.url}
+              alt={`preview ${index}`}
+              className='aspect-square h-full w-full object-cover' />
+          </div>
         ))
         }
       </div>
-    </>
+    </div>
   )
 }

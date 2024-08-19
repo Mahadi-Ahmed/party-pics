359f0d2f59ad298d4204161be19c1ebec9e85cdd
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-08-19 19:51:38 +0200

    use same icon for button & toaster during loading

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Toaster } from '@/components/ui/sonner.tsx'
+import { ReloadIcon } from '@radix-ui/react-icons'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Toaster />
+    <Toaster
+      icons={{
+        loading: <ReloadIcon className='h-4 w-4 animate-spin' />
+      }}
+    />
   </React.StrictMode>,
 )
